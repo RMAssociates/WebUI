e0c6e0b7fd4a74519e05662da7a11d2834114754
Raina <2raina@gmail.com>
2017-10-09 02:30:26 -0400

    Eslint and Carousel Init

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -4,11 +4,11 @@ import Header from './components/header.jsx';
 import style from './styles/main.scss';
 
 class App extends React.Component {
-  render () {
-    return <div>
-      <Header />
-       </div>
-  }
+	render () {
+		return <div>
+			<Header />
+		</div>;
+	}
 }
 
 render(<App/>, document.getElementById('app'));
